97f69ab4013ed0c623ca7314772d8b0d4c3577dd
Edward Gao <edsperanto@users.noreply.github.com>
2018-05-17 06:00:43 +0000

    add info to about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const About = () => {
 	return (
-		<div>
+		<div id='about'>
 			<h1>About Page</h1>
-			<p>This is the information about the "About Page"</p>
+			<p>This Kanban app was made using React and Redux on the front-end.</p>
+            <p>The backend was made using Node, Express, and Postgres.</p>
 		</div>
 	);
 }
